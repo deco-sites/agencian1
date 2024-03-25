73e83d5a456c5b60c6043107feb1d14ea6b9b926
rodolfo-ferreira <rodolfo.ferreira@agencian1.com.br>
2024-03-25 11:58:28 -0300

    style: add estilo na página de clientes + ajuste fonte no componente 'vamos dar vida'

--- sections/Content/TextWithImageCustom.tsx
+++ sections/Content/TextWithImageCustom.tsx
@@ -25,10 +25,10 @@ function TextWithImageCustom(
         md:px-[120px] relative flex items-center justify-between md:h-[540px]`)}
       >
         {textLarge && (
-          <h1 class={clx(`n1-text-width-custom__title mobile:text-[44px] mobile:leading-[56px] mobile:w-[80%] 
-            text-[90px] md:max-w-[60%] font-archimoto-medium font-black`)}>
+          <div class={clx(`n1-text-width-custom__title mobile:text-[44px] mobile:leading-[56px] mobile:w-[80%]
+            tablet:text-[60px] md:text-[90px] md:max-w-[60%] font-archimoto-medium font-black`)}>
             {textLarge}
-          </h1>
+          </div>
         )}
         {image && width && height && (
           <img
