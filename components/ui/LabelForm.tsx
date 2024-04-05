a0dbad3d29c06ec20c8b69551192065078c71ca8
rodolfo-ferreira <rodolfo.ferreira@agencian1.com.br>
2024-04-05 12:12:04 -0300

    add tooltip e ocultado links

--- components/ui/LabelForm.tsx
+++ components/ui/LabelForm.tsx
@@ -1,19 +1,19 @@
 import type { ComponentChildren } from "preact";
 
-interface Props{
-    nameAttr?:string;
-    _class?:string;
-    children?:ComponentChildren;
+interface Props {
+  nameAttr?: string;
+  _class?: string;
+  children?: ComponentChildren;
 }
 
-function LabelForm( { _class, nameAttr, children }:Props){
-    return(
-        <>
-            {_class && nameAttr && (
-                <label class={_class} htmlFor={nameAttr}> {children} </label>
-            )}
-        </>
-    )
+function LabelForm({ _class, nameAttr, children }: Props) {
+  return (
+    <>
+      {_class && nameAttr && (
+        <label class={_class} htmlFor={nameAttr}>{children}</label>
+      )}
+    </>
+  );
 }
 
 export default LabelForm;
