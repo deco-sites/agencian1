20c4faa1e5318ab2bf1fa4dd04fe24b52de7330a
Thiago Pereira <thiago@brandnexity.com>
2024-07-17 18:39:24 -0300

    Add CTACustom

--- components/ui/CTA.tsx
+++ components/ui/CTA.tsx
@@ -1,5 +1,6 @@
 import LinkTelephoneWithOptionArrow from "site/components/ui/LinkTelephoneWithOptionArrow.tsx";
 import { clx } from "$store/sdk/clx.ts";
+import { FnContext } from "deco/mod.ts";
 
 export interface CTAPhone {
   text?: string;
@@ -11,6 +12,19 @@ export interface CTAPhone {
   fontSize?: string;
 }
 export interface CTALayout {
+  /** @tilte Margin top */
+  /** @description Espaçamento entre uma section e outro ex:10px*/
+  marginTop?: string;
+  /** @description espaçamento entre uma section e outra ex:10px*/
+  marginTopMobile?: string;
+  /** @tilte Margin Bottom */
+  /** @description spaçamento entre uma section  e outro ex:10px*/
+  marginBottom?: string;
+  /** @description espaçamento entre uma section e outra ex:10px*/
+  marginBottomMobile?: string;
+  /** @description espaçamento entre uma section e outra ex:10px*/
+  marginLeft?: string;
+  marginLeftMobile?: string;
   backgroundColor?: string;
   customClass?: string;
 }
@@ -23,9 +37,27 @@ export interface Props {
   layout?: CTALayout;
 }
 
-function Cta({ buttonAds, layout }: Props) {
+function Cta({ buttonAds, layout, device }: ReturnType<typeof loader>) {
+  const { marginTop, marginTopMobile, marginBottom, marginBottomMobile, marginLeft, marginLeftMobile } = layout || {};
+  const isDesktop = device === "desktop";
+
   return (
-    <div class={clx(`${layout?.customClass ? layout?.customClass : ""}`)}>
+    <div
+      style={
+        isDesktop
+          ? {
+              marginTop: `${marginTop}`,
+              marginBottom: `${marginBottom}`,
+              marginLeft: `${marginLeft}`,
+            }
+          : {
+              marginTop: `${marginTopMobile || marginTop}`,
+              marginBottom: `${marginBottomMobile || marginBottom}`,
+              marginLeft: `${marginLeft || marginLeftMobile}}`,
+            }
+      }
+      class={clx(`${layout?.customClass ? layout?.customClass : ""}`)}
+    >
       <LinkTelephoneWithOptionArrow
         text={buttonAds?.text}
         telephone={buttonAds?.telephone}
@@ -38,5 +70,12 @@ function Cta({ buttonAds, layout }: Props) {
     </div>
   );
 }
+
+export const loader = (props: Props, _req: Request, ctx: FnContext) => {
+  return {
+    ...props,
+    device: ctx.device || "desktop",
+  };
+};
 
 export default Cta;
