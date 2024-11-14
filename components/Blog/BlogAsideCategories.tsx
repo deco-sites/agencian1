f66eee44be2d24ee3ed0b01bcdcaa3f588fdab52
Thiago Pereira <thiago@brandnexity.com>
2024-11-14 17:47:32 -0300

    Blog

--- components/Blog/BlogAsideCategories.tsx
+++ components/Blog/BlogAsideCategories.tsx
@@ -1,69 +1,110 @@
 import { clx } from "$store/sdk/clx.ts";
+import { AppContext } from "apps/blog/mod.ts";
+import { Category } from "apps/blog/types.ts";
+import { getRecordsByPath } from "apps/blog/utils/records.ts";
 
-interface ArrayCategory {
+export interface ArrayCategory {
   category?: string;
   count?: string;
+  slug?: string;
 }
 
-interface AsideCategories {
+export interface AsideCategories {
+  /**
+   * @title Nome do campo Categorias
+   * @format html
+   */
   title?: string;
   arrayCategories?: ArrayCategory[];
 }
 
-interface Props {
+export interface Props {
   categories?: AsideCategories;
 }
 
 function BlogAsideCategories({ categories }: Props) {
   return (
     <>
-      <div
-        class={clx(`flex flex-col gap-y-[20px]`)}
-      >
+      <div class={clx(`flex flex-col gap-y-[20px]`)}>
         {categories?.title && (
           <div
             class={clx(
-              `[&_*]:text-24 [&_*]:font-archimoto-medium [&_*]:font-black`,
+              `text-24 font-archimoto-medium font-black`
             )}
             dangerouslySetInnerHTML={{ __html: categories.title }}
-          >
-          </div>
+          ></div>
         )}
         {categories?.arrayCategories &&
           categories?.arrayCategories.length > 0 && (
-          <ul
-            class={clx(`flex flex-col gap-y-[10px]`)}
-          >
-            {categories?.arrayCategories.map((category) => {
-              return (
-                <>
-                  {category && (
-                    <li
-                      class={clx(`
+            <ul class={clx(`flex flex-col gap-y-[10px]`)}>
+              {categories?.arrayCategories.map((category) => {
+                return (
+                  <>
+                    {category && (
+                      <a href={`/nosso-blog?slug=${category?.slug}`}>
+                        <li
+                          class={clx(`
                                                 rounded-[30px] bg-[rgba(255,_255,_255,_0.10)] flex pl-[20px] 
                                                 pr-[8px] py-[8px] justify-between items-center font-noto-sans`)}
-                    >
-                      {category.category}
+                        >
+                          {category.category}
 
-                      {category?.count && (
-                        <span
-                          class={clx(`
+                          {category?.count && (
+                            <span
+                              class={clx(`
                                                         text-[#585858] text-14 leading-[22.4px] flex p-[7px] flex-col md:size-[36px]
                                                         justify-center items-center rounded-[30px] bg-secondary font-noto-sans `)}
-                        >
-                          {category.count}
-                        </span>
-                      )}
-                    </li>
-                  )}
-                </>
-              );
-            })}
-          </ul>
-        )}
+                            >
+                              {category.count}
+                            </span>
+                          )}
+                        </li>
+                      </a>
+                    )}
+                  </>
+                );
+              })}
+            </ul>
+          )}
       </div>
     </>
   );
 }
 
 export default BlogAsideCategories;
+
+export const loader = async (props: Props, _: Request, ctx: AppContext) => {
+  // const categories = await ctx.invoke.blog.loaders.Category();
+  // console.log("ddd", categories);
+
+  // const COLLECTION_PATH = "collections/blog/posts";
+  // const ACCESSOR = "post";
+  const COLLECTION_PATH = "collections/blog/categories";
+  const ACCESSOR = "category";
+
+  const categories = await getRecordsByPath<Category>(
+    ctx,
+    COLLECTION_PATH,
+    ACCESSOR
+  );
+
+  const arrayCategories: ArrayCategory[] = categories
+    .filter((n) => n !== undefined)
+    .map(({ name, slug }) => {
+      return {
+        category: name,
+        count: "1",
+        slug,
+      };
+    });
+
+  console.log(">>>>>>>>>>>>>>>>>>>", arrayCategories);
+
+  return {
+    ...props,
+    categories: {
+      ...props.categories,
+      arrayCategories,
+    },
+  };
+};
--- components/Blog/BlogAsideNewsletter.tsx
+++ components/Blog/BlogAsideNewsletter.tsx
@@ -1,41 +1,94 @@
 import { clx } from "$store/sdk/clx.ts";
+import type { JSX } from "preact";
 
-interface AsideNewsletter {
+export interface AsideNewsletter {
+  /**
+   * @title Nome do campo Newsletter
+   * @format html
+   */
   titleNewsletter?: string;
   maskNewsletterName?: string;
   maskNewsletterEmail?: string;
   textButton?: string;
 }
 
-interface Props {
+export interface Props {
   newsletter?: AsideNewsletter;
 }
 
 function BlogAsideNewsletter({ newsletter }: Props) {
+  const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    const { target } = e;
+    if (!target) return;
+
+    const email = (e.currentTarget.elements.namedItem("email") as RadioNodeList)
+      ?.value;
+    const name = (e.currentTarget.elements.namedItem("name") as RadioNodeList)
+      ?.value;
+
+    const news = e.currentTarget.closest(".news");
+    const inputElement = news?.querySelectorAll<HTMLInputElement>("input");
+    const loadingElement = news?.querySelector<HTMLElement>(".is-loading");
+    const message = news?.querySelector<HTMLElement>(".is-sucess");
+
+
+    if (email.trim().length && name.trim().length) {
+      try {
+        loadingElement?.classList.remove("hidden");
+        const response = await fetch("/api/newsletterform", {
+          method: "POST",
+          body: JSON.stringify({ email }),
+          headers: {
+            "content-type": "application/json",
+            accept: "application/json",
+          },
+        });
+
+        if (!response.ok) return;
+      } catch (err) {
+        loadingElement?.classList.add("hidden");
+        console.log("error: ", err.message);
+      } finally {
+
+        inputElement?.forEach((a) => a.value = "");
+        loadingElement?.classList.add("hidden");
+        e.currentTarget?.classList.add("hidden");
+
+        message?.classList.remove("hidden");
+        message?.classList.add("block");
+
+        setTimeout(() => {
+          e.currentTarget?.classList.remove("hidden");
+          message?.classList.add("hidden");
+          message?.classList.remove("block");
+        }, 5000);
+      }
+    }
+  };
   return (
-    <>
-      <form
-        class={clx(`flex flex-col gap-y-[20px]`)}
-      >
+    <div class="news">
+      <form class={clx(`flex flex-col gap-y-[20px]`)} onSubmit={handleSubmit}>
         {newsletter?.titleNewsletter && (
           <div
             name="email"
             htmlFor="email"
             class={clx(
-              `[&_*]:text-24 [&_*]:font-archimoto-medium [&_*]:font-black`,
+              `[&_*]:text-24 [&_*]:font-archimoto-medium [&_*]:font-black`
             )}
             dangerouslySetInnerHTML={{ __html: newsletter.titleNewsletter }}
-          >
-          </div>
+          ></div>
         )}
         {newsletter?.maskNewsletterName && (
           <input
-            id="email"
+            id="name"
+            name="name"
             type="text"
             placeholder={newsletter?.maskNewsletterName}
             class={clx(
               `rounded-[100px] py-[14px] px-[20px] max-h-[48px] bg-[#ffffff]
-                            font-normal text-primary text-[14px] leading-[18.2px] font-noto-sans outline-none`,
+                            font-normal text-primary text-[14px] leading-[18.2px] font-noto-sans outline-none`
             )}
           />
         )}
@@ -43,10 +96,11 @@ function BlogAsideNewsletter({ newsletter }: Props) {
           <input
             id="email"
             type="email"
+            name="email"
             placeholder={newsletter?.maskNewsletterEmail}
             class={clx(
               `rounded-[100px] py-[14px] px-[20px] max-h-[48px] bg-[#ffffff]
-                            font-normal text-primary text-[14px] leading-[18.2px] font-noto-sans outline-none`,
+                            font-normal text-primary text-[14px] leading-[18.2px] font-noto-sans outline-none`
             )}
           />
         )}
@@ -64,7 +118,11 @@ function BlogAsideNewsletter({ newsletter }: Props) {
           </div>
         )}
       </form>
-    </>
+      <div class="hidden is-loading"></div>
+        <span class="mt-4 is-sucess hidden font-noto-sans text-secondary mobile:text-14 md:text-16">
+          E-mail enviado com sucesso!
+        </span>
+    </div>
   );
 }
 
