8263ef87587bc90766536c865297c809466c6a76
Allisson <allisson.tagliaferro@n1.ag>
2024-12-05 14:03:58 -0300

    QA adjustments

--- components/Blog/PostContent.tsx
+++ components/Blog/PostContent.tsx
@@ -7,24 +7,15 @@ export default function PostContent({
   content: string;
   clamp?: number;
 }) {
-  // console.log(content);
   return (
     <div
       class={clx(
         "text-14 text-white font-noto-sans leading-6",
-        "[&_p:empty]:hidden [&_*]:!text-14 [&_*]:!leading-6",
-        "[&_span]:!text-white [&_p_span:first-child]:inline-block [&_p_span:first-child]:mt-4",
-        "[&_a]:!text-secondary [&_a_span]:!text-secondary",
+        "[&_p:empty]:hidden [&_*]:!leading-6 [&_>p:not(:last-child)]:mb-3",
         clamp ? `line-clamp-${clamp}` : "",
       )}
     >
-      <p dangerouslySetInnerHTML={{ __html: cleanContent(content) }} />
+      <p dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
-}
-
-function cleanContent(content: string) {
-  return content
-    .replaceAll("<p><br></p>", "")
-    .replaceAll("<p><p/>", "");
 }
